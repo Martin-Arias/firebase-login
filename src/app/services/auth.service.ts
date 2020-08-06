24a500180a675019197d45cca925104cd69faea1
Martin-Arias <arias95martin@gmail.com>
2020-08-05 21:41:08 -0300

    Validacion del token añadida

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -20,15 +20,18 @@ export class AuthService {
 
 
   constructor( private http: HttpClient) { 
+    //Llama a la funcion cuando se crea el componente 
     this.getToken();
   }
 
 
   logOut(){
-
+    //Remueve el token cuando se llama a la funcion
+    localStorage.removeItem('token');
   }
   logIn( usuario: UsuarioModel ){
 
+    //Iniciar sesion
     const authData = {
       email: usuario.email,
       password: usuario.password,
@@ -36,7 +39,7 @@ export class AuthService {
     };
     return this.http.post(`${ this.URL }:signInWithPassword?key=${ this.API_KEY }`,authData)
     .pipe(
-      map( resp => {
+      map( resp => {//Setea el token si los datos son correctos
         this.setToken( resp['idToken'] );
         return resp;
       })
@@ -44,7 +47,7 @@ export class AuthService {
 
   }
   createUser( usuario: UsuarioModel ){
-
+    //Registrar usuario
     const authData = {
       email: usuario.email,
       password: usuario.password,
@@ -52,7 +55,7 @@ export class AuthService {
     };
     return this.http.post(`${ this.URL }:signUp?key=${ this.API_KEY }`,authData)
     .pipe(
-      map( resp => {
+      map( resp => { //Setea el token si los datos son correctos
         this.setToken( resp['idToken'] );
         return resp;
       })
@@ -61,11 +64,20 @@ export class AuthService {
 
   private setToken( idToken: string ){
 
+    //Almacena el token en localStorage
     this.userToken = idToken;
     localStorage.setItem('token', idToken);
+
+    let loginTime = new Date(); //Guarda la hora al momento del inicio de sesion
+    loginTime.setSeconds( 3600 ) //Le suma 3600s/1h a la hora almacenada
+
+    localStorage.setItem('expiresIn', loginTime.getTime().toString() )
+
   }
 
   getToken(){
+
+    //Obtiene el token de LocalStorage
     if ( localStorage.getItem('token')) {
       this.userToken = localStorage.getItem('token')
     } else {
@@ -74,7 +86,29 @@ export class AuthService {
     return this.userToken
   }
 
+
+
   isAuthenticated():boolean{
-   return this.userToken.length > 2;
+    //Valida el token
+    if (this.userToken.length < 2) {
+      return false
+    }
+    //Transforma el valor a number
+    const expiresIn = Number(localStorage.getItem('expiresIn'));
+    
+    //Instancio un new Date(); para almacenar la hora que reciba de local storage
+    const expireTime = new Date();
+
+    //Setea la hora instanciada, con el valor recibido del local storage
+    expireTime.setTime(expiresIn);
+
+
+    //Si la hora de expiracion es mas grande que la hora actual devuelve true
+    if (expireTime > new Date()) {
+      return true;
+    }else {
+      return false
+    }
+
   }
 }
